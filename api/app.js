b5842747e69eeb383574ab55d8f2bea2d7770218
justen <justen@justens-MacBook-Pro.local>
2019-09-19 14:26:04 -1000

    connected to MongoDB successfully, able to GET, POST, and PATCH via Postman

--- api/app.js
+++ api/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+const { mongoose } = require("./db/mongoose");
+
+const bodyParser = require("body-parser");
+
+// Load in the mongoose models
+const { List, Task } = require("./db/models");
+
+// Load middleware
+app.use(bodyParser.json());
+
 /* ROUTE HANDLERS */
 
 /* LIST HANDLERS */
@@ -12,6 +22,9 @@ const app = express();
 
 app.get("/lists", (req, res) => {
   // Return an array of all the lists in the database
+  List.find({}).then(lists => {
+    res.send(lists);
+  });
 });
 
 /**
@@ -21,14 +34,31 @@ app.get("/lists", (req, res) => {
 app.post("/lists", (req, res) => {
   // Create a new list and return the new list document back to the user (which includes the id)
   // The list information (fields) will be passed in via the JSON request body
+  let title = req.body.title;
+
+  let newList = new List({
+    title
+  });
+  newList.save().then(listDoc => {
+    // the full list document is returned (including id)
+    res.send(listDoc);
+  });
 });
 
 /**
  * PATH /lists/:id
  * Purpose: Update a specified list
  */
-app.patch("list/:id", (req, res) => {
+app.patch("/lists/:id", (req, res) => {
   // Update the specified list (list document with id in the URL) with the new values specified in the JSON body of the request
+  List.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: req.body
+    }
+  ).then(() => {
+    res.sendStatus(200);
+  });
 });
 
 /**
