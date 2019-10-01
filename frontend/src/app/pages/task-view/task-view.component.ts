b2bc1fb334e57368ac3618d748954168f4cf2c20
justen <justen@justens-MacBook-Pro.local>
2019-09-30 15:03:02 -1000

    from the main page, able to click on + New List button to navigate to new page to create a list item based on List Name

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { TaskService } from "src/app/task.service";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
-  selector: 'app-task-view',
-  templateUrl: './task-view.component.html',
-  styleUrls: ['./task-view.component.scss']
+  selector: "app-task-view",
+  templateUrl: "./task-view.component.html",
+  styleUrls: ["./task-view.component.scss"]
 })
 export class TaskViewComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      console.log(params);
+    });
   }
-
 }
